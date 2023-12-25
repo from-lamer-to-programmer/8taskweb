7f6b52136a0c462f6092ce7b483c37a168c53014
Anton <53652055+from-lamer-to-programmer@users.noreply.github.com>
2023-12-25 10:26:54 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['email']) && isset($_POST['message'])) {
-    $to = "nik.fomenko005@mail.ru"; // Замените на вашу электронную почту
+    $to = "badaboombadaboom@mail.ru"; // Замените на вашу электронную почту
     $subject = "Новое сообщение с сайта";
     $email = $_POST['email'];
     $message = $_POST['message'];
