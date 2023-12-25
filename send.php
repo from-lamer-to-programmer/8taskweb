ca84a163b307f14a091c6c85795c4e6101502c4b
Anton <53652055+from-lamer-to-programmer@users.noreply.github.com>
2023-12-25 10:42:34 +0300

    f

--- send.php
+++ send.php
@@ -1,14 +1,18 @@
 <?php
-if(isset($_POST['email']) && isset($_POST['message'])) {
+if (isset($_POST['email']) && isset($_POST['message'])) {
     $to = "badaboombadaboom@mail.ru"; // Замените на вашу электронную почту
     $subject = "Новое сообщение с сайта";
     $email = $_POST['email'];
     $message = $_POST['message'];
     $headers = "From: $email";
-    
+
     // Отправка письма
-    mail($to, $subject, $message, $headers);
-    
-    echo "Письмо успешно отправлено!";
+    $sent = mail($to, $subject, $message, $headers);
+
+    if ($sent) {
+        echo "Письмо успешно отправлено!";
+    } else {
+        echo "Ошибка отправки письма.";
+    }
 }
 ?>
